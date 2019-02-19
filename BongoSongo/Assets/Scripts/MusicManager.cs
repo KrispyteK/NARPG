f504512041e41f44f75d50e4f3a2645af2262356
Blieper <yoranmandema@eml.cc>
2019-02-19 13:32:31 +0100

    MusicManager + Basic FFT

---
+++ BongoSongo/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour {
+
+
+    public Song Song;
+    public float Bass;
+
+    public static MusicManager Instance;
+
+    private AudioSource audioSource;
+    private float[] samples = new float[256];
+
+    void Awake () {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Debug.LogError("A MusicManager already exists in the scene!");
+        }
+    }
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = Song.Clip;
+        audioSource.Play();
+        audioSource.time = 72;
+    }
+
+    void Update() {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+
+        for (int i = 0; i < samples.Length; i++) {
+            Debug.DrawLine(new Vector3(i * 0.001f, 0), new Vector3(i * 0.001f, samples[i]), Color.cyan);
+        }
+    }
+}
---
+++ BongoSongo/Assets/Scripts/Song.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Song {
+    public AudioClip Clip;
+    public int BPM;
+}
