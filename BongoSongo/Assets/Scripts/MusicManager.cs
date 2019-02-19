85e5ca132571ef43d45b191169883a2f01141507
Blieper <yoranmandema@eml.cc>
2019-02-19 13:43:18 +0100

    Smooth out bass output from FFT

--- BongoSongo/Assets/Scripts/MusicManager.cs
+++ BongoSongo/Assets/Scripts/MusicManager.cs
@@ -8,11 +8,15 @@ public class MusicManager : MonoBehaviour {
 
     public Song Song;
     public float Bass;
+    public float spectrumBass;
+    public float spectrumBassMin = 0;
+    public float spectrumBassMax = 10;
 
     public static MusicManager Instance;
 
     private AudioSource audioSource;
-    private float[] samples = new float[256];
+    private float[] samples = new float[1024];
+
 
     void Awake () {
         if (Instance == null) {
@@ -33,8 +37,26 @@ public class MusicManager : MonoBehaviour {
     void Update() {
         audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
 
+        spectrumBass = 0;
+
         for (int i = 0; i < samples.Length; i++) {
             Debug.DrawLine(new Vector3(i * 0.001f, 0), new Vector3(i * 0.001f, samples[i]), Color.cyan);
+
+            if (i > spectrumBassMin && i < spectrumBassMax) {
+                spectrumBass += samples[i];
+            }
         }
+
+        spectrumBass /= (spectrumBassMax - spectrumBassMin);
+
+        float desiredBass = spectrumBass;
+
+        if (desiredBass > Bass) {
+            Bass = desiredBass;
+        } else {
+            Bass -= Bass * Time.deltaTime * 5f;
+        }
+
+        Debug.DrawLine(new Vector3(-0.005f, 0), new Vector3(-0.005f, Bass), Color.red);
     }
 }
