ab3f4e203d09ea91290d818ed3bb6a14b3d2261f
Blieper <yoranmandema@eml.cc>
2019-03-06 15:14:15 +0100

    Update WebcamMotion.cs

--- BongoSongo/Assets/Scripts/Webcam/WebcamMotion.cs
+++ BongoSongo/Assets/Scripts/Webcam/WebcamMotion.cs
@@ -5,12 +5,53 @@ using UnityEngine.UI;
 
 public class WebcamMotion : MonoBehaviour
 {
+    WebCamTexture webcamTexture;
+    RawImage rawImage;
+    AspectRatioFitter aspectRatioFitter;
+
     void Start()
     {
-        var webcamTexture = new WebCamTexture();
-        var rawImage = GetComponent<RawImage>();
+        webcamTexture = new WebCamTexture();
+        aspectRatioFitter = GetComponent<AspectRatioFitter>();
+        rawImage = GetComponent<RawImage>();
         rawImage.texture = webcamTexture;
         rawImage.rectTransform.sizeDelta = new Vector2(webcamTexture.width, webcamTexture.height);
         webcamTexture.Play();
+    }
+
+    void Update() {
+        if (webcamTexture.width< 100 ) {
+            Debug.Log("Still waiting another frame for correct info...");
+            return;
+        }
+
+        // change as user rotates iPhone or Android:
+
+        int cwNeeded = webcamTexture.videoRotationAngle;
+        // Unity helpfully returns the _clockwise_ twist needed
+        // guess nobody at Unity noticed their product works in counterclockwise:
+        int ccwNeeded = -cwNeeded;
+   
+        // IF the image needs to be mirrored, it seems that it
+        // ALSO needs to be spun. Strange: but true.
+        if (webcamTexture.videoVerticallyMirrored ) ccwNeeded += 180;
+   
+        // you'll be using a UI RawImage, so simply spin the RectTransform
+        rawImage.rectTransform.localEulerAngles = new Vector3(0f,0f, ccwNeeded);
+
+        float videoRatio = (float)webcamTexture.width / (float)webcamTexture.height;
+
+        // you'll be using an AspectRatioFitter on the Image, so simply set it
+        aspectRatioFitter.aspectRatio = videoRatio;
+   
+        // alert, the ONLY way to mirror a RAW image, is, the uvRect.
+        // changing the scale is completely broken.
+        if (webcamTexture.videoVerticallyMirrored )
+            rawImage.uvRect = new Rect(1,0,-1,1);  // means flip on vertical axis
+        else
+            rawImage.uvRect = new Rect(0,0,1,1);  // means no flip
+   
+        // devText.text =
+        //  videoRotationAngle+"/"+ratio+"/"+wct.videoVerticallyMirrored;
     }
 }
