ff0f436134bff61317ff26c5c12477d8204b61b7
KrypeK <yoranmandema@eml.cc>
2019-03-06 15:11:55 +0100

    webcam test

---
+++ BongoSongo/Assets/Scripts/Webcam/WebcamMotion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebcamMotion : MonoBehaviour
+{
+    void Start()
+    {
+        var webcamTexture = new WebCamTexture();
+        var rawImage = GetComponent<RawImage>();
+        rawImage.texture = webcamTexture;
+        rawImage.rectTransform.sizeDelta = new Vector2(webcamTexture.width, webcamTexture.height);
+        webcamTexture.Play();
+    }
+}
