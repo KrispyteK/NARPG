78ab0f65bc629b837a266c91ff21cb99c50eaf38
a-fr05t <45755253+a-fr05t@users.noreply.github.com>
2019-04-07 15:40:34 +0200

    SongSelection screen finished
    SongSelection screen finished. All assets implemented SelectionController script added

---
+++ BongoSongo/Assets/Scripts/SelectionController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionController : MonoBehaviour
+{
+    public GameObject[] songChoices;
+    public GameObject newSelection, toMove;
+    private int i = 0; //,iPrev;
+    public Transform leftArea; //,rightArea;
+
+    public void SelectionRight()
+    {
+        //iPrev = i;
+        toMove = songChoices[i];
+        i++;
+        if (i > 2)
+        {
+            i = 0;
+        }
+        newSelection = songChoices[i];
+        ChangePositions();
+    }
+
+      public void SelectionLeft()
+    {
+       // iPrev = i;
+        toMove = songChoices[i];
+        i--;
+        if (i < 0)
+        {
+            i = 2;
+        }
+        newSelection = songChoices[i];
+        ChangePositions();
+    }
+
+    public void ChangePositions()
+    {
+       newSelection.transform.position = new Vector3 (0, 0, 0);
+       toMove.transform.position = leftArea.position;
+     /*  if (iPrev < i)
+        {
+            toMove.transform.position = leftArea.position;
+        }
+
+       else if (iPrev < i)
+        {
+            toMove.transform.position = rightArea.position;
+        } */
+    }
+}
