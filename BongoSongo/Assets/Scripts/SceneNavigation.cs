577abe31340c17900496b789be55af9b0756de7a
a-fr05t <45755253+a-fr05t@users.noreply.github.com>
2019-04-07 12:41:18 +0200

    Menus created and functional
    New menu scenes added, Menu assets implemented. SceneNavigation script added to canvas of all menu screens. AnimatedTexture script added

---
+++ BongoSongo/Assets/Scripts/SceneNavigation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigation : MonoBehaviour
+{
+   public void StartDance()
+	{
+        SceneManager.LoadScene("WebCamTestScene");
+	}
+
+	public void SelectSong()
+	{   
+		SceneManager.LoadScene ("SongSelect");   
+	}
+
+	public void HomeScreen()
+	{
+		SceneManager.LoadScene ("HomeScreen");
+	}
+
+    public void EndScreen()
+	{
+		SceneManager.LoadScene ("EndScreen");
+	}
+
+    public void CloseApp()
+    {
+        // Function linked to close button on HomeScreen
+        Debug.Log ("Closing App");
+    }
+}
