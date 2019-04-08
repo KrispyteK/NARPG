bc1055dd56d697892de856291fbda20163b3fae7
Blieper <yoranmandema@eml.cc>
2019-04-08 17:03:45 +0200

    quit app logic

--- BongoSongo/Assets/Scripts/SceneNavigation.cs
+++ BongoSongo/Assets/Scripts/SceneNavigation.cs
@@ -3,31 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneNavigation : MonoBehaviour
-{
-   public void StartDance()
-	{
+public class SceneNavigation : MonoBehaviour {
+    public void StartDance() {
         SceneManager.LoadScene("DanceScene1");
-	}
+    }
 
-	public void SelectSong()
-	{   
-		SceneManager.LoadScene ("SongSelect");   
-	}
+    public void SelectSong() {
+        SceneManager.LoadScene("SongSelect");
+    }
 
-	public void HomeScreen()
-	{
-		SceneManager.LoadScene ("HomeScreen");
-	}
+    public void HomeScreen() {
+        SceneManager.LoadScene("HomeScreen");
+    }
 
-    public void EndScreen()
-	{
-		SceneManager.LoadScene ("EndScreen");
-	}
+    public void EndScreen() {
+        SceneManager.LoadScene("EndScreen");
+    }
 
-    public void CloseApp()
-    {
-        // Function linked to close button on HomeScreen
-        Debug.Log ("Closing App");
+    public void CloseApp() {
+        Application.Quit();
+        Debug.Log("Quiting");
+    }
+
+    void Update () {
+        if (Input.GetButtonDown("Cancel")) {
+            var scene = SceneManager.GetActiveScene();
+
+            switch (scene.name) {
+                case "HomeScreen":
+                    Application.Quit();
+                    Debug.Log("Quiting");
+                    break;
+                case "EndScreen":
+                case "SongSelect":
+                    HomeScreen();
+                    break;
+            }
+        }
     }
 }
