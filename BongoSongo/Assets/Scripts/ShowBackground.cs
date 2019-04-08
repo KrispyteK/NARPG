d7aa49c1ff97f528c96996ce38a64b7e83324cb7
KrypeK <yoranmandema@eml.cc>
2019-04-08 12:01:45 +0200

    android builds + tests

--- BongoSongo/Assets/Scripts/ShowBackground.cs
+++ BongoSongo/Assets/Scripts/ShowBackground.cs
@@ -10,12 +10,26 @@ public class ShowBackground : MonoBehaviour {
     }
 
     void SetTexture() {
-        var webcamTexture = showMotionCapture ? WebcamMotionCapture.instance.renderTexture : (Texture)WebcamMotionCapture.instance.webcamTexture;
+        var webcamTexture = WebcamMotionCapture.instance.webcamTexture;
+        var motionTexture = WebcamMotionCapture.instance.renderTexture;
 
-        GetComponent<Renderer>().material.mainTexture = webcamTexture;
+        FloatingText.Create(Vector2.zero, "" + webcamTexture.videoRotationAngle, 10f);
 
-        var aspect = (float)webcamTexture.width / (float)webcamTexture.height;
+        var useTexture = showMotionCapture ? motionTexture : (Texture)webcamTexture;
+
+        GetComponent<Renderer>().material.mainTexture = useTexture;
+
+        var rotationAngle = webcamTexture.videoRotationAngle;
+        //var rotationAngle = 270;
+
+        var isTurned = rotationAngle == 90 || rotationAngle == 270;
+
+        var aspect = isTurned ? (float)useTexture.height / (float)useTexture.width : (float)useTexture.width / (float)useTexture.height;
 
         transform.localScale = new Vector3(-1 / aspect, 1, 1);
+
+        var rotation = Quaternion.LookRotation(-Vector3.up, -Vector3.forward) * Quaternion.Euler(Vector3.up * -rotationAngle);
+
+        transform.rotation = rotation;
     }
 }
