cd687a672c360723e4471dda462bbf6704423f59
Blieper <yoranmandema@eml.cc>
2019-05-19 12:33:40 +0200

    Use scriptable object asset for gameplay settings

--- BongoSongo/Assets/Scripts/GameManager.cs
+++ BongoSongo/Assets/Scripts/GameManager.cs
@@ -21,7 +21,7 @@ public class GameManager : MonoBehaviour {
     public void AddScore(int add) {
         score += add;
 
-        InterScene.instance.score = score;
+        InterScene.Instance.score = score;
     }
 
     void Update() {
---
+++ BongoSongo/Assets/Scripts/GamePlaySettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlaySettings : ScriptableObject {
+
+    public float indicatorScale = 0.13f;
+
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("Assets/Create/GamePlaySettings")]
+    public static void CreateAsset() {
+        var asset = ScriptableObject.CreateInstance<GamePlaySettings>();
+
+        UnityEditor.AssetDatabase.CreateAsset(asset, "Assets/Resources/Settings/NewGamePlaySettings.asset");
+        UnityEditor.AssetDatabase.SaveAssets();
+
+        UnityEditor.EditorUtility.FocusProjectWindow();
+
+        UnityEditor.Selection.activeObject = asset;
+    }
+#endif
+}
