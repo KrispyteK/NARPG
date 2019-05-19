221c9a2973e5f4c5e666dbda8d9620326004b449
Blieper <yoranmandema@eml.cc>
2019-05-19 12:33:00 +0200

    Start work on editor scene

---
+++ BongoSongo/Assets/Scripts/EditorScene/SetSpriteSize.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSpriteSize : MonoBehaviour {
+
+    public Transform sprite;
+
+    void Start() {
+        sprite.localScale = CameraTransform.Scale(Vector3.one * InterScene.Instance.gamePlaySettings.indicatorScale);
+    }
+}
