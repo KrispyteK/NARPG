34087974c12f4bd4b844e39abbf0d95c04801339
Blieper <yoranmandema@eml.cc>
2019-06-03 19:44:31 +0200

    use scroll view for song select screen

---
+++ BongoSongo/Assets/Scripts/UI/SongSelectContent.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SongSelectContent : MonoBehaviour {
+
+    public GameObject songPanel;
+
+    void Start() {
+        GenerateButtons();
+    }
+
+    public void GenerateButtons() {
+        foreach (Transform child in transform) {
+            Destroy(child.gameObject);
+        }
+
+        var files = new List<string>();
+
+        files.AddRange(Directory.GetFiles(Application.streamingAssetsPath, "*.level", SearchOption.AllDirectories));
+
+        foreach (var file in files) {
+            var panel = Instantiate(songPanel, transform);
+
+            panel.GetComponentInChildren<Text>().text = Path.GetFileName(file).Replace(".level", "");
+        }
+    }
+}
