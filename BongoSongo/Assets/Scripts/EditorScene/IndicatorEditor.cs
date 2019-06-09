3b02a634bd183de06be3c05f163b827e56d28833
Blieper <yoranmandema@eml.cc>
2019-06-09 16:38:17 +0200

    improved selecting/dragging usage

--- BongoSongo/Assets/Scripts/EditorScene/IndicatorEditor.cs
+++ BongoSongo/Assets/Scripts/EditorScene/IndicatorEditor.cs
@@ -8,6 +8,7 @@ public class IndicatorEditor : MonoBehaviour {
 
     public int pixelDragThreshold = 2;
     public bool isDragging;
+    public bool startedClickOnSelected;
 
     Vector3 offset;
     int selectDepth = 0;
@@ -18,16 +19,14 @@ public class IndicatorEditor : MonoBehaviour {
     void Update() {
 
         if (Input.GetMouseButtonDown(0)) {
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+
             isDragging = false;
             canDrag = false;
 
-            if (EventSystem.current.IsPointerOverGameObject()) {
-                //EditorManager.instance.Unselect();
-
-                return;
-            }
-
             beginMousePos = Input.mousePosition;
+
+            startedClickOnSelected = false;
 
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
@@ -37,54 +36,42 @@ public class IndicatorEditor : MonoBehaviour {
 
             if (selectDepth >= hits.Length) selectDepth = 0;
 
-            if (!hits.Any()) {
-                EditorManager.instance.Unselect();
-                return;
-            }
+            if (!hits.Any()) return;
 
             hit = hits[selectDepth];
             var target = hit.collider.gameObject;
             var selector = target.GetComponent<Selector>();
-
-            offset = target.transform.position - mousePos;
-
-            //if (lastSelected != null) { 
-            //    var lastSelectedIsInHits = hits.ToList().Find(x => x.collider.gameObject == lastSelected?.gameObject) != default && false;
-
-            //    if (selector == lastSelected || lastSelectedIsInHits) {
-            //        print("Hit same");
-
-            //        canDrag = true;
-
-            //        return;
-            //    }
-            //}
 
             if (hit.collider != null) {
                 EditorManager.instance.Unselect();
 
                 selector.Select();
 
+                lastSelected = selector;
+
                 EditorManager.instance.selected = target.GetComponent<Selector>();
 
-                lastSelected = target.GetComponent<Selector>();
+                startedClickOnSelected = lastSelected == EditorManager.instance.selected;
 
                 selectDepth++;
-            }
-            else if (EditorManager.instance.selected) {
-                EditorManager.instance.Unselect();
             }
         }
 
         if (EditorManager.instance.selected != null && Input.GetMouseButton(0)) {
             Vector2 mousePos = Input.mousePosition;
 
-            if ((beginMousePos - mousePos).magnitude > pixelDragThreshold && !EventSystem.current.IsPointerOverGameObject()) {
+            if ((beginMousePos - mousePos).magnitude > pixelDragThreshold && !EventSystem.current.IsPointerOverGameObject() && !isDragging) {
+                offset = EditorManager.instance.selected.transform.position - Camera.main.ScreenToWorldPoint(mousePos);
+
                 isDragging = true;
             }
         }
 
         if (Input.GetMouseButtonUp(0)) {
+            if (EditorManager.instance.selected && !isDragging && !startedClickOnSelected) {
+                EditorManager.instance.Unselect();
+            }
+
             isDragging = false;
             canDrag = false;
         }
