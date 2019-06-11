237755caf4014be30510785e4c0bcc22127d65c0
Blieper <yoranmandema@eml.cc>
2019-06-11 17:16:36 +0200

    use box collider for slider  editor

--- BongoSongo/Assets/Scripts/EditorScene/Slider/SliderHandles.cs
+++ BongoSongo/Assets/Scripts/EditorScene/Slider/SliderHandles.cs
@@ -8,38 +8,27 @@ public class SliderHandles : MonoBehaviour {
     public Transform handleTransform;
 
     private CurveRenderer curveRenderer;
-    private PolygonCollider2D polyCollider;
+    private BoxCollider2D boxCollider;
 
     void Start() {
         curveRenderer = GetComponent<CurveRenderer>();
-        polyCollider = GetComponent<PolygonCollider2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update() {
         var points = new List<Vector3>();
-        var points2D = new List<Vector2>();
 
-        Vector2 min = Vector2.zero;
-        Vector2 max = Vector2.zero;
+        var bounds = new Bounds(handleTransform.GetChild(0).position, Vector3.zero);
 
         foreach (Transform child in handleTransform) {
             //child.GetComponent<SliderHandleSelector>().slider = gameObject;
 
             points.Add(child.position);
-            points2D.Add(child.position);
 
-            if (child.localPosition.x < min.x) min.x = child.localPosition.x - 0.1f;
-            if (child.localPosition.y < min.y) min.y = child.localPosition.y - 0.1f;
-            if (child.localPosition.x > max.x) max.x = child.localPosition.x + 0.1f;
-            if (child.localPosition.y > max.y) max.y = child.localPosition.y + 0.1f;
+            bounds.Encapsulate(child.position);
         }
 
         curveRenderer.points = points.ToArray();
-        polyCollider.points = new Vector2[] {
-            min,
-            new Vector2(min.x,max.y),
-            max,
-            new Vector2(max.x,min.y),
-            };
+        boxCollider.size = bounds.size;
     }
 }
