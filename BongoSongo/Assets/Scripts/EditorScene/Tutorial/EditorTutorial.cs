397dad0e19e5c6a077a3f743c58ba13f117a9eea
Blieper <yoranmandema@eml.cc>
2019-06-12 23:25:52 +0200

    show message when there's no custom levels

---
+++ BongoSongo/Assets/Scripts/EditorScene/Tutorial/EditorTutorial.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorTutorial : MonoBehaviour {
+
+    public GameObject NoLevels;
+
+    void Update() {
+
+    }
+}
