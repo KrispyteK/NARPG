da1bf5c4f4a793fe9297d6f918ec4113a83480c2
Blieper <yoranmandema@eml.cc>
2019-06-12 12:34:52 +0200

    Add GoogleMobileAds plugin

---
+++ BongoSongo/Assets/Scripts/Ads/AdsInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdsInitializer : MonoBehaviour {
+    public void Start() {
+#if UNITY_ANDROID
+        string appId = "ca-app-pub-8099181658913762~8623879374";
+#else
+        string appId = "unexpected_platform";
+#endif
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+    }
+}
---
+++ BongoSongo/Assets/Scripts/Ads/DisplayInterstitialAd.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class DisplayInterstitialAd : MonoBehaviour
+{
+    private InterstitialAd interstitial;
+
+    public void RequestInterstitial() {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+    }
+}
