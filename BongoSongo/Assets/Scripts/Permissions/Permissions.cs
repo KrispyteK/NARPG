6f0c4d2b203dea53c375f6a3758633bbb2b7a0ca
KrypeK <yoranmandema@eml.cc>
2019-06-17 14:17:16 +0200

    ask for permissions before app start

---
+++ BongoSongo/Assets/Scripts/Permissions/Permissions.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_ANDROID
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class Permissions : MonoBehaviour {
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBeforeSceneLoadRuntimeMethod() {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera)) {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+
+        if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite)) {
+            Permission.RequestUserPermission(Permission.ExternalStorageWrite);
+        }
+    }
+}
+#endif
