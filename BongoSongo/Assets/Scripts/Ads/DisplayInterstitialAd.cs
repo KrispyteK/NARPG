80bf3b662a3492c1720489f6ecacfcab558cb08d
KrypeK <yoranmandema@eml.cc>
2019-06-17 16:24:21 +0200

    Getting stuff ready for release

--- BongoSongo/Assets/Scripts/Ads/DisplayInterstitialAd.cs
+++ BongoSongo/Assets/Scripts/Ads/DisplayInterstitialAd.cs
@@ -38,10 +38,16 @@ public class DisplayInterstitialAd : MonoBehaviour
         string adUnitId = "unexpected_platform";
 #endif
 
+        //Bundle extras = new Bundle();
+        //extras.putString("max_ad_content_rating", "G");
+        //AdRequest request = new AdRequest.Builder().addNetworkExtrasBundle(AdMobAdapter.class, extras) .tagForChildDirectedTreatment(true) .build();
+
         // Initialize an InterstitialAd.
         this.interstitial = new InterstitialAd(adUnitId);
         // Create an empty ad request.
         AdRequest request = new AdRequest.Builder()
+            .AddExtra("max_ad_content_rating", "G")
+            .TagForChildDirectedTreatment(true)
             .AddTestDevice("fa5a74be9c07b043dd89da89f58b2bb6".ToUpper())
             .Build();
         // Load the interstitial with the request.
